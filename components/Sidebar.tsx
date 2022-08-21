cfdb26897074a1158391e1bad7574b3a4351f462
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-08-21 21:50:44 +0500

    implementing google oauth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
 
@@ -41,7 +40,7 @@ const Sidebar = () => {
 						<div className='px-2 py-4 hidden xl:block'>
 							<p className='text-gray-400'>Log in to like and comment on videos</p>
 							<div className='pr-4'>
-								<GoogleLogin
+								{/* <GoogleLogin
 									render={(renderProps) => (
 										<button
 											className='cursor-pointer bg-white text-lg text-[#F51997] border=[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]'
@@ -54,7 +53,7 @@ const Sidebar = () => {
 									onSuccess={() => {}}
 									onFailure={() => {}}
 									cookiePolicy='single_host_origin'
-								/>
+								/> */}
 							</div>
 						</div>
 					)}
